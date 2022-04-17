cd2ca2ebb35c0ad7e9809c982c999900070043ed
Ashok Nayak <ashok.nayakii@gmail.com>
2022-04-16 20:44:08 -0400

    dirname required at server.js saving notes to db solved

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+_dirname = fs.realpathSync('.');
+_dirname = path.resolve();
 
 app.use(express.static("public"));
 // parse incoming string or array data
@@ -15,8 +17,16 @@ app.use(express.json());
 
 // HTML ROUTES TO CREATE
 
+// API ROUTES TO CREATE
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+app.get("/api/notes", (req, res) => {
+  res.json(noteLogs.slice(1));
+});
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
+  res.sendFile(path.join(_dirname, './public/index.html'));
 })
 
 // GET /notes should return the notes.html file
@@ -31,38 +41,36 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-function addNote(body, noteArray) {
-  const newNote = body;
-  if (!Array.isArray(noteArray))
-  noteArray = [];
 
-  if (noteArray.length === 0)
-  noteArray[0]++;
+function addNote(body, notesArray) {
+  const note = body;
+  if (!Array.isArray(notesArray))
+  notesArray = [];
 
-  noteArray.push(newNote);
+  if (notesArray.length === 0)
+  notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(note);
   fs.writeFileSync(
       path.join(_dirname, './db/db.json'),
-      JSON.stringify(noteArray, null, 2)
+      JSON.stringify(notesArray, null, 2)
   );
-  return newNote;
+  return note;
 }
 
-
-// API ROUTES TO CREATE
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
-
-app.get("/api/notes", (req, res) => {
-  res.json(noteLogs.slice(1));
-});
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 // You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.post('/api/notes', (req, res) => {
-    const newNote = addNote(req.body, noteLogs);
-    res.json(newNote);
+  const note = addNote(req.body, noteLogs);
+  res.json(note);
 })
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
 });
+
